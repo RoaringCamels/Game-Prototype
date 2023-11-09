2277509ec6a603ebda07ce25ccc2ccd4e4b458c5
Ivan Revilla <revillaivan97@gmail.com>
2023-11-08 23:06:24 -0800

    Added Cooldown to shooting

--- 2D Test/Assets/Scripts/Player/Pivot.cs
+++ 2D Test/Assets/Scripts/Player/Pivot.cs
@@ -7,24 +7,29 @@ public class Pivot : MonoBehaviour
     public Transform gun_holder;
     public Transform fire_point;
     public GameObject bullet;
-
-    public float shootCooldown;
     public AudioSource audioSource;
     public AudioClip shootingAudioClip;
+
+    private float nextShootTime = 0.0f; // Time when the next shot is allowed
+    public float shootCooldown = 0.5f;
+
     private void Update()
     {
         RotateGunTowardsMouse();
-        if (Input.GetMouseButtonDown(0))
+
+        // Check if the left mouse button is held down and the cooldown time has passed.
+        if (Input.GetMouseButton(0) && Time.time >= nextShootTime)
         {
             PlayerShoot();
+            nextShootTime = Time.time + shootCooldown;
         }
     }
 
     void RotateGunTowardsMouse()
     {
-        if (UnityEngine.Camera.main != null)
+        if (Camera.main != null)
         {
-            Vector2 mousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 direction = mousePosition - (Vector2)gun_holder.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
