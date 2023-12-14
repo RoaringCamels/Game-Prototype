bfd6119035fba360aca04a261cb206741390fcd1
Ivan Revilla <revillaivan97@gmail.com>
2023-12-13 17:52:16 -0800

    Fixed Merge conflicts and combined the scenes

--- 2D Test/Assets/Scripts/Enemies/BasicEnemy.cs
+++ 2D Test/Assets/Scripts/Enemies/BasicEnemy.cs
@@ -5,19 +5,12 @@ public class BasicEnemy : MonoBehaviour
     public Animator enemyAnimator; // Reference to the Animator component.
 
     private bool isDead = false;
-    public int hitPoints = 3;
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!isDead && collision.gameObject.CompareTag("Bullet"))
         {
-            // Count the hits
-            hitPoints--;
-
-            // Check the hitCount
-            if (hitPoints <= 0){
-                // Trigger the "IsDead" animation.
+            // Trigger the "IsDead" animation.
             enemyAnimator.SetTrigger("IsDead");
 
             // Set the "isDead" flag to true to prevent further collisions.
@@ -29,7 +22,6 @@ public class BasicEnemy : MonoBehaviour
             // Calculate the length of the "IsDead" animation and destroy the object after that duration.
             float animationLength = enemyAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
             Destroy(gameObject, animationLength);
-            }
         }
     }
 }
